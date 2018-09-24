b3fc66d998a6be43805f392251327d2e231e8780
clooooode <jackey8616@gmail.com>
2018-09-24 20:29:43 +0800

    Remove annotation

--- tgbot/__init__.py
+++ tgbot/__init__.py
@@ -4,8 +4,6 @@ from threading import Event
 import telepot
 from telepot.loop import MessageLoop
 
-#from entity import Entity
-#from room import Room
 from utils import *
 from command import Command
 from command.bind import Bind
@@ -20,12 +18,6 @@ class TgBot(threading.Thread):
     def loadPersonConfig(self):
         persons = {}
         return persons
-
-#    def loadRoomConfig(self):
-#        rooms = {}
-#        for (key, value) in self.config['rooms']:
-#            rooms[key] = Room(value)
-#        return rooms
 
     def __init__(self, config, tunnels, loopDelay=0.1):
         super(TgBot, self).__init__()
@@ -46,7 +38,6 @@ class TgBot(threading.Thread):
             Toggle('toggle')
         ]
         self.persons = self.loadPersonConfig()
-#        self.rooms = self.loadRoomConfig()
 
     def sendMessage(self, _id, _msg):
         self.bot.sendMessage(_id, _msg)
@@ -56,19 +47,6 @@ class TgBot(threading.Thread):
         try:
             _from = msg['from']
             _chat = msg['chat']
-#            This comment is for @(metions) and /(command) detection, but haven't appropriately code yet.
-#            Just leave these for commit use and will be remove at next commit.
-#            if 'entities' in msg:  # Mention or Command in msg
-#                _entities = Entity.fromDict(msg['entities'])
-#                if _entities[0].type == 'mention' and _entities[0].fetchData(msg) == self.botUserName:  # Mention AsukaMeow
-#                    if _entities[1].type == 'bot_command':  # Mention and type command
-#                        pass
-#                    else:  # Mention and type message
-#                        pass
-#                elif _entities[0].type == 'bot_command':  # Commands
-#                    pass
-#                else:   # Others
-#                    pass
             if msg['text'].startswith('/'):
                 for each in self.commands:
                     if msg['text'].startswith('/' + each.cmd): #msg['text'].startswith('/' + type(each).__name__.lower()):
